7c1af286c0108d749a06de8968de4de63cd71e3a
solimanghandour <[200~solimanghandour21@gmail.com>
2023-04-15 06:20:16 +0200

    jquery_hide

---
+++ main.js
@@ -0,0 +1,17 @@
+
+//this is js //
+
+
+
+// function hide(){
+//     document.getElementById("div1").style.display="none"
+// }
+
+
+
+
+//this is jquery//
+$('#div1').click( function hide(){
+
+  $('#div1').hide()
+})
